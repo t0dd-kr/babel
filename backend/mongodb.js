76c0847fef334145196c430603d304b3d8b7ec19
t0dd-kr <swjasontodd@gmail.com>
2018-02-13 04:26:08 +0900

    Update heroku mongodb

--- backend/mongodb.js
+++ backend/mongodb.js
@@ -2,8 +2,8 @@ const DB_NAME = 'babel'
 var mongoose = require('mongoose');
 var dbconnection = function() {
 
-  var mongoDB = 'mongodb://127.0.0.1:27017/' + DB_NAME;
-  // var mongoDB = process.env.MONGODB_URI
+  // var mongoDB = 'mongodb://127.0.0.1:27017/' + DB_NAME;
+  var mongoDB = process.env.MONGODB_URI
   mongoose.connect(mongoDB);
   mongoose.Promise = global.Promise;
   var db = mongoose.connection;
